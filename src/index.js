2df8cb0797ca24fee11e6e6e182c73df3fd7be7c
Петър Стоев <petarstoev0505@gmail.com>
2022-12-06 00:13:02 +0200

    v1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
   // Used to access the app instance by the automated tests
+  
   app.setEmojis(["🐒", "🦍", "🦧"]);
-  app.addBananas();
+  let emojis=app.emojis
+  let banana=app.banana
+  app.addBananas(emojis.map(element => element + banana));
+
+  let ee=  emojis.map(element => element + banana)
+  
+ let paragraph= document.createElement("paragraph")
+ paragraph.textContent = ee
+ 
+ document.querySelector("#emojis").appendChild(paragraph)
+ console.log()
   window.__JS_APP = app;
+
 });
